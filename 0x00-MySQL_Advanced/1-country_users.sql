270d964e4c9eb63b9522e46021f351378acbc4ea
Shy Manuel <manuelshylet@gmail.com>
2023-10-12 02:38:57 -0700

    solution to task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- A SQL script that create table users with the following requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
